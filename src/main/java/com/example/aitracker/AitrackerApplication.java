546ffd9245fa1457224b060cefa594aa73ea0e73
Tathagata Mishra <tathagatamishra007@gmail.com>
2026-05-28 18:54:45 +0530

    init

---
+++ src/main/java/com/example/aitracker/AitrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.aitracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AitrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AitrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aitracker/AitrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aitracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
